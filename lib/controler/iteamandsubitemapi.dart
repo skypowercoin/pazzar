6d510aca71c6c47f6c7742adfa5998143f65eafd
skypowercoin <osamaattal2009@gmail.com>
2022-05-27 19:08:29 +0000

    remove uneused file

--- lib/controler/iteamandsubitemapi.dart
+++
@@ -1,7 +0,0 @@
-///https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/add sub iteam  or iteam link here.;'knnnb==
-///
-
-import 'package:http/http.dart';
-import 'dart:convert' as convert;
-
-import 'package:http/http.dart' as http;
